714584359663c9519f606bae4be5cdd48bdb8464
Alexandro Brugnoni <Alexandro Brugnoni>
2022-11-16 08:46:25 +0100

    BussaIsNiga

--- comuni/Comuni.java
+++ comuni/Comuni.java
@@ -2,10 +2,9 @@ package comuni;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -44,31 +43,55 @@ public class Comuni extends Application {
 		try {
 			FileReader flussoCaratteri = new FileReader("C:\\Users\\alexandrobrugnoni\\Desktop\\comuni.txt");
 			BufferedReader lettoreDiRighe = new BufferedReader(flussoCaratteri);
-			
-			FileOutputStream fileOp = new FileOutputStream("C:\\Users\\alexandrobrugnoni\\Desktop\\comuni.csv");
-			OutputStreamWriter flussoCaratteriOp = new OutputStreamWriter(fileOp,"UTF-8");
-			
+
+			String txtOutput;
+			FileWriter fwOutput = new FileWriter("C:\\Users\\alexandrobrugnoni\\Desktop\\comuni.csv");
+
 			String rigaLetta;
 			String codIstat, comune, provincia, cap, codCatastale;
+			char charArray[];
+			boolean isSpazio = true;
+
 			do {
-			    rigaLetta = lettoreDiRighe.readLine();
-			    if(rigaLetta!=null) {
-			    	codIstat = rigaLetta.substring(0,4);
-			    	comune = rigaLetta.substring(4,20);
-			    	comune = comune.toLowerCase();
-			    	comune = comune
-			    	provincia = rigaLetta.substring(54,56);
-			    	cap = rigaLetta.substring(56,61);
-			    	codCatastale = rigaLetta.substring(61,65);
-			    	
-			    	System.out.println(codIstat + "," + comune + "," + provincia + "," + cap + "," + codCatastale); 
-			    	flussoCaratteriOp.write(codIstat + "," + comune + "," + provincia + "," + cap + "," + codCatastale);
-			    	flussoCaratteriOp.close();
-			    	fileOp.close();
-			    }
+				rigaLetta = lettoreDiRighe.readLine();
+				if(rigaLetta!=null) {
+					codIstat = rigaLetta.substring(0,4);
+
+					comune = rigaLetta.substring(4,54).toLowerCase();
+					charArray = comune.toCharArray();
+
+					for(int i = 0; i < charArray.length; i++) {
+						// if the array element is a letter
+						if(Character.isLetter(charArray[i])) {
+							// check isSpazio is present before the letter
+							if(isSpazio) {
+								// change the letter into uppercase
+								charArray[i] = Character.toUpperCase(charArray[i]);
+								isSpazio = false;
+							}
+						}
+
+						else {
+							// if the new character is not character
+							isSpazio = true;
+						}
+					}
+					
+					comune = String.valueOf(charArray).trim();
+					provincia = rigaLetta.substring(54,56);
+
+					cap = rigaLetta.substring(56,61);
+
+					codCatastale = rigaLetta.substring(61,65);
+
+					txtOutput = codIstat + "," + comune + "," + provincia + "," + cap + "," + codCatastale;
+					System.out.println(codIstat + "," + comune + "," + provincia + "," + cap + "," + codCatastale); 
+					fwOutput.write(txtOutput+"\n");
+				}
 			}while(rigaLetta!=null);        
 			lettoreDiRighe.close();
 			flussoCaratteri.close();
+			fwOutput.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch(IOException e2) {
